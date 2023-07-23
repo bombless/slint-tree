c52e71de9f5dd646349deab1bcc959d22922d4db
York Xiang <bombless@126.com>
2023-07-23 16:49:31 +0800

    Draw layout myself

--- src/lib.rs
+++ src/lib.rs
@@ -19,7 +19,7 @@ pub fn main(fonts : HashMap<&str, &Object>) {
 
     for (idx, o) in fonts {
         let id = acc.len() as i32;
-        acc.push(view(id, 0., format!("{idx:?}:")));
+        acc.push(view(id, 0., format!("{idx:?}:"), false));
         parse_value_helper(10., id, Dict(o.dict().clone()), &mut acc, &mut make_track);
     }
     
@@ -39,7 +39,18 @@ pub fn main(fonts : HashMap<&str, &Object>) {
         for child in children {
             let mut row = list.row_data(child as _).unwrap();
             row.hide = open;
+            row.open = !open;
             list.set_row_data(child as _, row);
+        }
+
+        let mut height = 0.;
+        for i in 0 .. list.row_count() {
+            let mut row = list.row_data(i).unwrap();
+            row.y = height;
+            if !row.hide {
+                height += 16.;
+            }
+            list.set_row_data(i, row);
         }
     });
 
@@ -57,8 +68,8 @@ fn all_children(source: &HashMap<i32, Vec<i32>>, id: i32, acc: &mut Vec<i32>) {
     }
 }
 
-fn view(id: i32, indent: f32, text: String) -> ViewData {
-    ViewData { id, text: text.into(), indent, hide: false, open: true }
+fn view(id: i32, indent: f32, text: String, leaf: bool) -> ViewData {
+    ViewData { id, text: text.into(), x: indent, y: id as f32 * 16., hide: false, open: true, leaf }
 }
 
 fn parse_value_helper(indent: f32, parent: i32, value: parser::Value, acc: &mut Vec<ViewData>, track: &mut dyn FnMut(i32, i32)) {
@@ -66,23 +77,23 @@ fn parse_value_helper(indent: f32, parent: i32, value: parser::Value, acc: &mut 
     let id = acc.len() as i32;
     track(id, parent);
     match value {
-        Number(n) => acc.push(view(id, indent, n.to_string())),
-        String(s) => acc.push(view(id, indent, format!("String({s:?})"))),
-        Key(s) => acc.push(view(id, indent, format!("Key({s:?})").into())),
+        Number(n) => acc.push(view(id, indent, n.to_string(), true)),
+        String(s) => acc.push(view(id, indent, format!("String({s:?})"), true)),
+        Key(s) => acc.push(view(id, indent, format!("Key({s:?})").into(), true)),
         List(l) => {
-            acc.push(view(id, indent, "list:".into()));
+            acc.push(view(id, indent, "list:".into(), false));
             for x in l {
                 parse_value_helper(indent + 10., id, x, acc, track);
             }
         }
-        Ref(x, y) => acc.push(view(id, indent, format!("Ref({x}, {y})"))),
+        Ref(x, y) => acc.push(view(id, indent, format!("Ref({x}, {y})"), true)),
         Dict(m) => {
-            acc.push(view(id, indent, format!("dict")));
+            acc.push(view(id, indent, format!("dict"), false));
             let parent = id;
             for (k, v) in m {
                 let key_id = acc.len() as _;
                 track(key_id, parent);
-                acc.push(view(key_id, indent + 10., format!("{k}:")));
+                acc.push(view(key_id, indent + 10., format!("{k}:"), false));
                 parse_value_helper(indent + 20., key_id, v, acc, track);
             }
         }
@@ -90,13 +101,15 @@ fn parse_value_helper(indent: f32, parent: i32, value: parser::Value, acc: &mut 
 }
 
 slint::slint! {
-    import { ScrollView, VerticalBox } from "std-widgets.slint";
+    import { ScrollView, VerticalBox, TabWidget } from "std-widgets.slint";
     export struct ViewData {
         id: int,
         text: string,
-        indent: length,
+        x: length,
+        y: length,
         open: bool,
         hide: bool,
+        leaf: bool,
     }
     
     export component View inherits Window {
@@ -105,25 +118,23 @@ slint::slint! {
         callback toggle(int);
         in property <[ViewData]> items;
         ScrollView {
-            VerticalBox {
-                for item in root.items: HorizontalLayout {
-                    if !item.hide: Rectangle {
-                        width: item.indent;
-                    }
-                    if !item.hide: Text {
+            for item in root.items: Rectangle {
+                y: item.y;
+                height: 16px;
+                if !item.hide && !item.leaf: Rectangle {
+                    x: 16px;
+                    width: item.x;
+                    height: 16px;
+                    Text {
                         text: item.open ? "↓" : "→";
-                        horizontal-alignment: right;
-                        vertical-alignment: center;
-                        width: 20px;
                         TouchArea {
                             clicked => { toggle(item.id) }
                         }
                     }
-                    if !item.hide: Text {
-                        horizontal-alignment: left;
-                        text: item.text;
-                    }
-                    
+                }
+                if !item.hide: Text {
+                    x: item.x + 16px;
+                    text: item.text;
                 }
             }
         }
